360ea7cb8bf7f95f0e54c5842df5e9849aa4b9e4
AliceGazizullina <kaskamalpgka@gmail.com>
2026-03-31 01:43:19 +0500

    Добавила простой генератор

--- password_generation/generator/views.py
+++ password_generation/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -7,5 +8,13 @@ def home(request):
     return render(request,'generator/home.html', {'password':'Qwerty1234'})
 
 def password(request):
-    return render(request,'generator/password.html')
+    characters = list('abcdefghijklmnoprstquvwyxz')
+
+    length = 10
+
+    thepassword = ''
+    for i in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request,'generator/password.html', {'password':thepassword})
 
